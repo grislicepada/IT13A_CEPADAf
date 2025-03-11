c90bf61a49fa46562fcd80657cf5c448583ee8e7
grislicepada <graceliemaecepada3@gmail.com>
2025-03-11 08:10:23 +0800

    Added Palindrome

---
+++ IT13A_DEV/MIDTERM/Palindrome.java
@@ -0,0 +1,34 @@
+
+package MIDTERM;
+
+import java.util.Scanner;
+public class Palindrome {
+    public static void main (String[] args){
+    
+        Scanner scanner = new Scanner (System.in);
+        boolean u = true;
+        while (u){
+        String word = "";
+        String newWord = "";
+        
+        System.out.println("Enter a word:");
+           word = scanner.nextLine();
+           
+         int i = word.length()-1;
+         
+        do {
+            newWord = newWord + word.charAt(i);
+            i--;
+            
+        } while (i>=0);
+        
+        if (newWord.equals(String.valueOf(word))){
+               System.out.println("Palindrome.");
+        
+        }else{
+                System.out.println("Not a Palindrome.");
+                }
+    
+        }
+    }
+}
